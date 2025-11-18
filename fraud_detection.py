706bf0dd782e01691c07a1b8f1609ec1048a6ae8
AlejandroJB18 <100454261@alumnos.uc3m.es>
2025-11-18 20:16:39 +0100

    cambios repositorio

--- fraud_detection.py
+++
@@ -1,2 +0,0 @@
-from src.compare_models import compare_all_factors
-compare_all_factors('data/creditcard.csv')
--- src/train_model.py
+++ src/train_model.py
@@ -6,6 +6,7 @@ Cost-sensitive trainers: RandomForest & XGBoost.
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
 
 def train_rf_with_cost(X_train, y_train, amount_train, amount_factor=10):
     """
@@ -24,6 +25,31 @@ def train_rf_with_cost(X_train, y_train, amount_train, amount_factor=10):
     rf.fit(X_train, y_train, sample_weight=weights)
     print(f"RandomForest trained (factor={amount_factor})")
     return rf
+
+def train_lgbm_with_cost(X_train, y_train, amount_train, amount_factor=10):
+    """
+    Train LightGBM with fraud amount as sample weight.
+    """
+    weights = np.ones(len(y_train))
+    fraud = y_train == 1
+    weights[fraud] = amount_train[fraud] * amount_factor
+
+    # 2. Configurar el modelo LightGBM
+    lgbm = LGBMClassifier(
+        n_estimators=200,
+        max_depth=6,
+        learning_rate=0.1,
+        random_state=42,
+        n_jobs=-1,
+        objective='binary',          # Especificamos que es clasificación binaria
+        metric='binary_logloss',     # Equivalente a 'logloss' de XGBoost
+        verbose=-1                   # Reduce los warnings de consola
+    )
+
+
+    lgbm.fit(X_train, y_train, sample_weight=weights)
+    print(f"LightGBM trained (factor={amount_factor})")
+    return lgbm
 
 def train_xgb_with_cost(X_train, y_train, amount_train, amount_factor=10):
     """
